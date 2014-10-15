7285b036a6d721eac46e2aee5c9f1a6758f1be97
Fabiano <fabiano.pavan@soluzionipa.it>
2014-10-15 16:41:03 +0200

    Settato a true il single sign out del cas

--- apps/cas_server/config/options.rb
+++ apps/cas_server/config/options.rb
@@ -6,6 +6,7 @@ module Spider
     config_option 'cas.proxy_granting_ticket_expiry', :type => Fixnum, :default => 48*60*60
     config_option 'cas.ticket_granting_ticket_expiry', :type => Fixnum, :default => 48*60*60
     config_option 'cas.saml1_1_compatible', :type => Spider::DataTypes::Bool, :default => false
+    config_option 'cas.enable_single_sign_out', :type => Spider::DataTypes::Bool, :default => true
     config_option 'cas.saml_compliant_tickets', :type => String, :choices => [false, '1', '2', '4'], :default => lambda{
         Spider.conf.get('cas.saml1_1_compatible') ? '1' : false
     }
