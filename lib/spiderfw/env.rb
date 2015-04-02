73b3a7a5b44765fcbd7735ee26e79fb63f81dbfd
loris <loris.pizzolitto@soluzionipa.it>
2015-04-02 10:54:21 +0200

    modifica encoding per ruby versioni >= 1.9

--- lib/spiderfw/env.rb
+++ lib/spiderfw/env.rb
@@ -1,6 +1,6 @@
 RUBY_VERSION_PARTS = RUBY_VERSION.split('.')
 ENV['LC_CTYPE'] = 'en_US.UTF-8'
-if RUBY_VERSION =~ /1.9/  
+if RUBY_VERSION >= "1.9"  
 	Encoding.default_external = Encoding::UTF_8  
 	#Encoding.default_internal = Encoding::UTF_8  
 end 
