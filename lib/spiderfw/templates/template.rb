e6d7cbcd74461181b7d93aa92c67369d180b74f6
fabiano <fabiano.pavan@soluzionipa.it>
2015-11-16 10:48:13 +0100

    Fatto il convert_object sulla scene del template.rb per non avee problemi con caratteri strani

--- lib/spiderfw/templates/template.rb
+++ lib/spiderfw/templates/template.rb
@@ -713,6 +713,7 @@ module Spider
                 prev_domain = Spider::GetText.set_domain(td)
             end
             scene ||= @scene
+            scene = scene.convert_object
             load unless loaded?
             init(scene) unless init_done?
             exec
