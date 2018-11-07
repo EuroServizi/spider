2ff3fc96bf6ab5e2711caab7da496a9d939c83df
loris <loris.pizzolitto@soluzionipa.it>
2018-11-07 14:40:38 +0100

    Revert "Tolta forzatura di Encoding durante salvataggio files uploadati"
    This reverts commit 07b2ec430d14677085e32e874904a3e6e6f9dd10.

--- lib/spiderfw/http/http.rb
+++ lib/spiderfw/http/http.rb
@@ -433,8 +433,10 @@ module Spider
             @encoding = encoding
             if RUBY_VERSION =~ /^1.9.3/
                 super('uploaded', Spider.paths[:tmp], content_type, :encoding => encoding)
-            else
+            elsif RUBY_VERSION =~ /^1.8.7/ #caso con ruby 1.8.7
                 super('uploaded', Spider.paths[:tmp])
+            else
+                super('uploaded', Spider.paths[:tmp], :encoding => encoding)
             end
         end
         
